0c9015fdab3e1e5f049e2f69d22f9c25e86e17c6
Harun Yahya <bisnisharuny@gmail.com>
2023-09-11 20:23:19 +0700

    Update dasar Python.py
    syntax untuk membuat fungsi sederhana mencari rata-rata pada data di dalam list

--- dasar Python.py
+++ dasar Python.py
@@ -7,4 +7,22 @@ while angka <=100:
   print(angka)
   angka +=1
 
+# Membuat fungsi sederhana
+"""
+  Membuat fungsi untuk menentukan rata-rata
+"""
+def hitung_mean(daftar_angka):
+    if len(daftar_angka) == 0:
+        return 0  # Menghindari pembagian oleh nol jika daftar kosong.
+    
+    total = sum(daftar_angka)
+    mean = total / len(daftar_angka)
+    return mean
+
+# Contoh penggunaan fungsi
+angka = [6, 10, 18, 29, 33]
+rata_rata = hitung_mean(angka)
+print(f"Mean dari {angka} adalah {rata_rata}")
+
+
 
